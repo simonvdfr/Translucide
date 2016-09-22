91504e2b4fb6c41be5155a39b590bf111a78eca0
simonvdfr <simonvd@hotmail.fr>
2016-09-22 02:40:50 +0200

    Renvoi un code postal et la ville à l'autocomplet

---
+++ plugin/ville-cp.php
@@ -0,0 +1,19 @@
+<?
+@include_once("../config.php");// Variables
+//include_once("../api/fonction.php");// Fonctions
+include_once("../api/db.php");// Connexion à la db
+
+$term = $connect->real_escape_string($_GET["term"]);
+
+$sel = $connect->query("SELECT ville_code_commune, ville_nom_reel, ville_code_postal FROM villes_france_free WHERE ".($_GET['type'] == "ville" ? "ville_nom_simple" : "ville_code_postal")." LIKE '".$term."%' LIMIT 50");
+while($res = $sel->fetch_assoc())
+{
+	$data[] = array('id' => $res['ville_code_commune'], 'label' => $res['ville_nom_reel'], 'value' => $res['ville_code_postal']);
+}
+
+header("Content-Type: application/json; charset=utf-8");
+
+echo json_encode($data);
+
+$connect->close();
+?>
