3f0f8c8f8487b23d8806dff8b2d08471ea9e0784
simonvdfr <simonvd@hotmail.fr>
2016-09-22 02:41:32 +0200

    Correction du message afficher une fois l'envoi fini. Envoi d'un mail à l'admin avec les infos de l'inscrit

--- plugin/alert-mail.php
+++ plugin/alert-mail.php
@@ -10,26 +10,6 @@ if($_POST["email"] and !$_POST["champ_vide"])// champ_vide pour éviter les bots 
 
 		if(filter_var($email, FILTER_VALIDATE_EMAIL))// Email valide
 		{
-			/*$subject = "[".$GLOBALS['sitename']."] ".htmlspecialchars($_POST["email"]);
-
-			$message = nl2br(strip_tags($_POST["message"]));
-
-			$message .= "<br /><br />-------------------------------------------------------<br />";
-
-			if($_POST['referer']) $message .= "Referer : ".htmlspecialchars($_POST['referer'])."<br />";
-
-			$message .= "IP du Visiteur : ".getenv("REMOTE_ADDR")."<br />";
-			$message .= "Host : ".gethostbyaddr($_SERVER["REMOTE_ADDR"])."<br />";		
-			$message .= "User Agent : ".getenv("HTTP_USER_AGENT")."<br />";
-			$message .= "IP du Serveur : ".getenv("SERVER_ADDR")."<br />";
-
-			$header="Content-type:text/html; charset=utf-8\r\nFrom:".($_POST["email"] ? htmlspecialchars($_POST["email"]) : $GLOBALS['email_contact']);
-
-			if(mail($GLOBALS['email_contact'], $subject, stripslashes($message), $header))*/
-
-
-			//CREATE TABLE IF NOT EXISTS `".$GLOBALS['db_prefix']."email` (`email` varchar(100) NOT NULL, `nom` varchar(100) NOT NULL, `tel` varchar(50) NOT NULL, `ip` varchar(16) NOT NULL, `date` datetime NOT NULL, UNIQUE KEY `email` (`email`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 			include_once("../api/fonction.php");
 			include_once("../api/db.php");
 
@@ -38,12 +18,40 @@ if($_POST["email"] and !$_POST["champ_vide"])// champ_vide pour éviter les bots 
 			$email = $GLOBALS['connect']->real_escape_string($email);
 			$ip = $GLOBALS['connect']->real_escape_string(ip());
 
+			$subject = "[".$GLOBALS['sitename']."] Inscription alerte mail ".htmlspecialchars($email);
+
+			$message .= htmlspecialchars($nom)."<br />".htmlspecialchars($email)."<br />".htmlspecialchars($tel)."<br />";
+
+			$message .= "<br /><br />-------------------------------------------------------<br />";
+
+			$message .= "IP du Visiteur : ".getenv("REMOTE_ADDR")."<br />";
+			$message .= "Host : ".gethostbyaddr($_SERVER["REMOTE_ADDR"])."<br />";		
+			$message .= "User Agent : ".getenv("HTTP_USER_AGENT")."<br />";
+			$message .= "IP du Serveur : ".getenv("SERVER_ADDR")."<br />";
+
+			$header="Content-type:text/html; charset=utf-8\r\nFrom:".($email ? htmlspecialchars($email) : $GLOBALS['email_contact']);
+
+			mail($GLOBALS['email_contact'], $subject, stripslashes($message), $header);
+
+
+			//CREATE TABLE IF NOT EXISTS `".$GLOBALS['db_prefix']."email` (`email` varchar(100) NOT NULL, `nom` varchar(100) NOT NULL, `tel` varchar(50) NOT NULL, `ip` varchar(16) NOT NULL, `date` datetime NOT NULL, UNIQUE KEY `email` (`email`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
 			if($GLOBALS['connect']->query("REPLACE INTO ".$GLOBALS['db_prefix']."email SET email='".($email)."', nom='".($nom)."', tel='".($tel)."', ip='".($ip)."', date=NOW()"))
 			{
 				?>
 				<script>
-				light(__("We will keep you informed of progress"));
-				//$("#alertmail").fadeOut();
+				
+				</script>
+				<script>
+				$(document).ready(function()
+				{
+					//light(__("We will keep you informed of progress"));
+					//$("#alertmail").fadeOut();
+					$("#alertmail").fadeOut("400", function(){
+						$("#alertmail").html("<h2>"+ __("We will keep you informed of progress") +" <i class='fa fa-check checked'></i></h2>").fadeIn();
+					});
+				});
 				</script>
 				<?
 			}
@@ -61,6 +69,7 @@ if($_POST["email"] and !$_POST["champ_vide"])// champ_vide pour éviter les bots 
 }
 else// Affichage du formulaire
 {
+
 	if(!$GLOBALS['microtime']) exit;
 	?>
 
@@ -84,7 +93,7 @@ else// Affichage du formulaire
 
 		<input type="hidden" name="nonce_alertmail" value="<?=nonce("nonce_alertmail");?>">
 
-		<button class="mbs w50"><i class="fa fa-fw fa-envelope"></i> <?_e("M'avertir de l'avancée de ".$GLOBALS['sitename'])?></button>
+		<button class="mbs w50"><span><i class="fa fa-fw fa-envelope"></i> <?_e("M'avertir de l'avancée de ".$GLOBALS['sitename'])?></span></button>
 
 	</form>
 
