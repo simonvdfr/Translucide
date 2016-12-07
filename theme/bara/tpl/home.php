e12068802f5886428de37a9a814d3c5287ea11ac
simonvdfr <simonvd@hotmail.fr>
2016-12-07 20:59:49 +0100

    Changement animation de la poussière

--- theme/bara/tpl/home.php
+++ theme/bara/tpl/home.php
@@ -6,7 +6,7 @@
 	<h2><?txt("h2")?></h2>
 	<div style="background-image: url(media/zverre.png);" class="zverre"></div>
 	<div style="background-image: url(media/zburger.png);" class="zburger"></div>
-	<div style="background-image: url(media/zdust.png);" class="zdust"></div>
+	<div style="background-image: url(media/zdust.jpg);" class="zdust"></div>
 	<a href="#scrolldown"><span></span></a>
 </section>
 
@@ -30,7 +30,7 @@
 
 <section class="parallax mod mtl mbm pal tc white animation fade-in" <?bg('bg-2')?>>
 	<h2 class=""><?txt('titre-2')?></h2>
-	<h3 class="medium mbl"><?txt('sstitre-2')?></h3>
+	<h3 class="big mbl"><?txt('sstitre-2')?></h3>
 </section>
 
 
@@ -61,7 +61,7 @@
 
 <section class="parallax mod mtl mbm pal tc white animation fade-in" <?bg('bg-5')?>>
 	<h2 class=""><?txt('titre-5')?></h2>
-	<h3 class="medium mbl"><?txt('sstitre-5')?></h3>
+	<h3 class="big mbl"><?txt('sstitre-5')?></h3>
 </section>
 
 
@@ -84,6 +84,60 @@
 		<p class="mrm"><?txt('txt-7')?></p>
 
 		<div class="link-box"><?txt('link-7')?></div>
+
+	</article>
+
+</section>
+
+
+<section class="parallax mod mtl mbm pal tc white animation fade-in" <?bg('bg-8')?>>
+	<h2 class=""><?txt('titre-8')?></h2>
+	<h3 class="big mbl"><?txt('sstitre-8')?></h3>
+</section>
+
+
+<section class="mw960p mod center">		
+
+	<article class="w50 fl mbm animation slide-left">
+
+		<h2 class="mbn"><?txt('titre-9')?></h2>
+		
+		<p class="mrm"><?txt('txt-9')?></p>
+
+	</article>
+
+	<article class="w50 fl mbm plm animation slide-right">
+
+		<h2 class="mbn"><?txt('titre-10')?></h2>
+		
+		<p class="mrm"><?txt('txt-10')?></p>
+
+	</article>
+
+</section>
+
+
+<section class="parallax mod mtl mbm pal tc white animation fade-in" <?bg('bg-11')?>>
+	<h2 class=""><?txt('titre-11')?></h2>
+	<h3 class="big mbl"><?txt('sstitre-11')?></h3>
+</section>
+
+
+<section class="mw960p mod center mbl">		
+
+	<article class="w50 fl mbm animation slide-left">
+
+		<h2 class="mbn"><?txt('titre-12')?></h2>
+		
+		<p class="mrm"><?txt('txt-12')?></p>
+
+	</article>
+
+	<article class="w50 fl mbm plm animation slide-right">
+
+		<h2 class="mbn"><?txt('titre-13')?></h2>
+		
+		<p class="mrm"><?txt('txt-13')?></p>
 
 	</article>
 
