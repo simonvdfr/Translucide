48d53f6a383937903f1069ca9903023c74bec76d
simonvdfr <simonvd@hotmail.fr>
2017-02-02 13:01:01 +0100

    Ajout de la template Katona Night

---
+++ theme/katona-night/tpl/home.php
@@ -0,0 +1,31 @@
+<?if(!$GLOBALS['domain']) exit;?>
+
+<section class="mw960p mod center">
+
+	<img src="/theme/katona-night/img/flower.png">
+
+	<h1 class="tc"><?txt('titre-1')?></h1>
+
+</section>
+
+
+<div id="cloud"></div>
+
+
+<section id="paper" class="relative">		
+	<article class="mw960p center">
+
+		<div id="moon" class="no-small-screen"></div>
+
+		<?txt('txt')?>
+		
+	</article>
+</section>
+
+
+<div id="hills"></div>
+
+<div id="wave"></div>
+<div id="hill-green"></div>
+<div id="wave-light"></div>
+<div id="wave" class="seconde"></div>
