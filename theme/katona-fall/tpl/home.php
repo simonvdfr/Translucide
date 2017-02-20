1317c1360a3063ccd2303d397be4ee6da38d464c
simonvdfr <simonvd@hotmail.fr>
2017-02-20 17:22:05 +0100

    Ajout des éléments manquant au thème fall

---
+++ theme/katona-fall/tpl/home.php
@@ -0,0 +1,26 @@
+<?if(!$GLOBALS['domain']) exit;?>
+
+<section class="mw960p mod center">
+
+	<img src="/theme/katona-fall/img/leaf.png">
+
+	<h1 class="tc"><?txt('titre-1')?></h1>
+
+</section>
+
+
+<div id="cloud"></div>
+
+
+<section id="paper" class="relative">		
+	<article class="mw960p center"><?txt('txt')?></article>
+</section>
+
+
+<div id="hills"></div>
+
+<div id="wave"></div>
+<div id="hill-green"></div>
+<div id="wave-light"></div>
+<div id="wave" class="seconde"></div>
+<div id="wave-light" class="seconde"></div>
---
+++ theme/katona-fall/translation.php
@@ -0,0 +1,5 @@
+<?
+$add_translation = array(
+	"Send" => array("fr" => "Envoyer")	
+);
+?>
