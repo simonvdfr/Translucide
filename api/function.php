d3062cdb84e9ac5f510806441d69933c46085993
simonvdfr <simonvd@hotmail.fr>
2017-10-28 17:47:00 +0200

    ajout du masquage du champ tag

--- api/function.php
+++ api/function.php
@@ -444,7 +444,7 @@ function tag($key = null, $filter = array())
 {
 	$key = encode($key ? $key : "tag");
 
-	echo'<'.(isset($filter['tag']) ? $filter['tag'] : "div").' id="'.$key.'" class="editable-tag">';
+	echo'<'.(isset($filter['tag']) ? $filter['tag'] : "div").' id="'.$key.'" class="editable-tag'.(isset($filter['class']) ? " ".$filter['class'] : "").'">';
 
 	$i = 1;
 	$sel_tag = $GLOBALS['connect']->query("SELECT * FROM ".$GLOBALS['table_meta']." WHERE id='".(int)$GLOBALS['id']."' AND type='tag' ORDER BY ordre ASC LIMIT 10");
@@ -453,7 +453,7 @@ function tag($key = null, $filter = array())
 		$GLOBALS['tags'][$res_tag['cle']] = $res_tag['val'];
 
 		if($i > 1) echo', ';
-		echo'<a href="'.make_url($key, array($res_tag['cle'], 'domaine' => true)).'" class="tdn'.(isset($filter['class']) ? " ".$filter['class'] : "").'">'.$res_tag['val'].'</a>';
+		echo'<a href="'.make_url($key, array($res_tag['cle'], 'domaine' => true)).'" class="tdn">'.$res_tag['val'].'</a>';
 		$i++;
 	}
 
