652134d46a42f605ac1987a88e0c6f02ed93b8dc
simonvdfr <simonvd@hotmail.fr>
2017-12-11 15:23:26 +0100

    Ajout de la possibilité d'ajaxé une page en l'appelant par son URL. Détection que l'on est dans le de l'Ajax pour ne pas afficher toutes les metas et head

--- index.php
+++ index.php
@@ -12,6 +12,14 @@ if(stristr($_SERVER['REQUEST_URI'], 'index.php')){
 }
 
 
+// On ajax une page ?
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) and strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+	$ajax = true;
+else
+	$ajax = false;
+
+
+
 // Sélectionne la langue
 $lang = get_lang();
 
@@ -97,42 +105,6 @@ if(isset($GLOBALS['content']['og-image'])) $image = $GLOBALS['content']['og-imag
 
 
 
-/********** MENU DE NAVIGATION **********/
-
-// On récupère les données de la navigation
-$sel_nav = $connect->query("SELECT * FROM ".$table_meta." WHERE type='nav' AND cle='".$lang."' LIMIT 1");
-$res_nav = $sel_nav->fetch_assoc();
-
-// Extraction du menu
-if($res_nav['val']) $GLOBALS['nav'] = json_decode($res_nav['val'], true);
-else $GLOBALS['nav'] = array();
-
-
-
-/********** HEADER **********/
-
-// On récupère les données du header
-$sel_header = $connect->query("SELECT * FROM ".$table_meta." WHERE type='header' AND cle='".$lang."' LIMIT 1");
-$res_header = $sel_header->fetch_assoc();
-
-// Ajout des données du footer
-if($res_header['val'])
-	$GLOBALS['content'] = @array_merge($GLOBALS['content'], json_decode($res_header['val'], true));
-
-
-
-/********** FOOTER **********/
-
-// On récupère les données du footer
-$sel_footer = $connect->query("SELECT * FROM ".$table_meta." WHERE type='footer' AND cle='".$lang."' LIMIT 1");
-$res_footer = $sel_footer->fetch_assoc();
-
-// Ajout des données du footer
-if($res_footer['val'])
-	$GLOBALS['content'] = @array_merge($GLOBALS['content'], json_decode($res_footer['val'], true));
-
-
-
 /********** TAGS **********/
 
 // Construction de l'ajout du tag/cat au title et get info
@@ -178,129 +150,176 @@ if(isset($GLOBALS['function']) and $GLOBALS['function'] != "")
 	include_once($_SERVER["DOCUMENT_ROOT"].$GLOBALS['path']."theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"").$GLOBALS['function']);
 
 
+// Si pas ajax on charge toute la page
+if(!$ajax)
+{
+	/********** MENU DE NAVIGATION **********/
 
-header('Content-type: text/html; charset=UTF-8');
+	// On récupère les données de la navigation
+	$sel_nav = $connect->query("SELECT * FROM ".$table_meta." WHERE type='nav' AND cle='".$lang."' LIMIT 1");
+	$res_nav = $sel_nav->fetch_assoc();
 
-?><!DOCTYPE html>
-<html lang="<?=$lang;?>">
-<head>
-	
-	<meta charset="utf-8">
-
-	<title><?=$title;?></title>
-
-	<?if($description){?><meta name="description" content="<?=$description;?>"><?}?>
-
-	<meta name="robots" content="<?=$robots;?>">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<meta property="og:site_name" content="<?=utf8_encode($GLOBALS['sitename']);?>">
-	<meta property="og:title" content="<?=$title;?>">
-	<meta property="og:type" content="website">
-	<?if(isset($res['url'])){?><meta property="og:url" content="<?=make_url($res['url'], array("domaine" => true))?>"><?}?>
-	
-	<?if($description){?><meta property="og:description" content="<?=$description;?>"><?}?>
-	<?if($image){?><meta property="og:image" content="<?=$GLOBALS['home'].$image;?>"><?}?>
-
-	<?if(isset($GLOBALS['facebook_api_id'])){?><meta property="fb:app_id" content="<?=$GLOBALS['facebook_api_id'];?>"><?}?>
-	
-	<?if(isset($GLOBALS['google_page'])){?><link href="<?=$GLOBALS['google_page'];?>" rel="publisher" /><?}?>
+	// Extraction du menu
+	if($res_nav['val']) $GLOBALS['nav'] = json_decode($res_nav['val'], true);
+	else $GLOBALS['nav'] = array();
 
 
-	<?if(isset($GLOBALS['icons'])){?><link rel="stylesheet" href="<?=$GLOBALS['icons']?>"><?}?>
 
-	<link rel="stylesheet" href="<?=$GLOBALS['path']?>api/global<?=$GLOBALS['min']?>.css?<?=$GLOBALS['cache']?>">	
+	/********** HEADER **********/
 
-	<link rel="stylesheet" href="<?=$GLOBALS['path']?>theme/<?=$GLOBALS['theme'].($GLOBALS['theme']?"/":"")?>style<?=$GLOBALS['min']?>.css?<?=$GLOBALS['cache']?>">	
+	// On récupère les données du header
+	$sel_header = $connect->query("SELECT * FROM ".$table_meta." WHERE type='header' AND cle='".$lang."' LIMIT 1");
+	$res_header = $sel_header->fetch_assoc();
 
-
-	<?if(isset($GLOBALS['touch_icon'])){?><link rel="apple-touch-icon" href="<?=$GLOBALS['touch_icon'];?>"/><?}?>
-
-	<link rel="shortcut icon" type="image/x-icon" href="<?=$GLOBALS['path']?>media/favicon.ico?<?=$GLOBALS['cache']?>">
-
-
-	<script src="<?=$GLOBALS['jquery']?>"></script>
-
-	<script src="<?=$GLOBALS['path']?>api/lucide.init<?=$GLOBALS['min']?>.js?<?=$GLOBALS['cache']?>"></script>
+	// Ajout des données du footer
+	if($res_header['val'])
+		$GLOBALS['content'] = @array_merge($GLOBALS['content'], json_decode($res_header['val'], true));
 
 
-	<script>
-		<? if(isset($GLOBALS['google_analytics']) and $GLOBALS['google_analytics']) { ?>
-		// Google Analytics
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', '<?=$GLOBALS['google_analytics'];?>', 'auto');
-		ga('send', 'pageview');
-		<? }
 
-		if(isset($GLOBALS['facebook_sdk']) and $GLOBALS['facebook_api_id']) { ?>
-		// Facebook
-		(function(d, s, id){
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {return;}
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.7&cookie=true&appId=<?=$GLOBALS['facebook_api_id'];?>";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-		<? } 
+	/********** FOOTER **********/
 
-		if(isset($_COOKIE['autoload_edit']) and $_SESSION['auth']['edit-page']){?>
-			// Si demande l'autoload du mode édition et si admin
-			$(function(){
-				edit_launcher();
-				$("a.bt.fixed.edit").fadeOut();				
-			});
-			<?
-			// Supprime le cookie qui demande de charger automatiquement l'admin
-			@setcookie("autoload_edit", "", time() - 3600, $GLOBALS['path'], $GLOBALS['domain']);
-		}?>			
+	// On récupère les données du footer
+	$sel_footer = $connect->query("SELECT * FROM ".$table_meta." WHERE type='footer' AND cle='".$lang."' LIMIT 1");
+	$res_footer = $sel_footer->fetch_assoc();
 
-		// Variables
-		id = "<?=$id?>";
-		state = "<?=(isset($res['state'])?$res['state']:"")?>";
-		permalink = "<?=(isset($res['url'])?$res['url']:"")?>";
-		type = "<?=(isset($res['type'])?$res['type']:"")?>";
-		tpl = "<?=(isset($res['tpl'])?$res['tpl']:"")?>";
-		tag = "<?=(isset($tag)?encode($tag):"")?>";
-		path = "<?=$GLOBALS['path']?>";
-		theme = "<?=$GLOBALS['theme']?>";
-	</script>
+	// Ajout des données du footer
+	if($res_footer['val'])
+		$GLOBALS['content'] = @array_merge($GLOBALS['content'], json_decode($res_footer['val'], true));
 
 
-	<!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
 
-</head>
-<body>
+	header('Content-type: text/html; charset=UTF-8');
+
+	?><!DOCTYPE html>
+	<html lang="<?=$lang;?>">
+	<head>
+		
+		<meta charset="utf-8">
+
+		<title><?=$title;?></title>
+
+		<?if($description){?><meta name="description" content="<?=$description;?>"><?}?>
+
+		<meta name="robots" content="<?=$robots;?>">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<meta property="og:site_name" content="<?=utf8_encode($GLOBALS['sitename']);?>">
+		<meta property="og:title" content="<?=$title;?>">
+		<meta property="og:type" content="website">
+		<?if(isset($res['url'])){?><meta property="og:url" content="<?=make_url($res['url'], array("domaine" => true))?>"><?}?>
+		
+		<?if($description){?><meta property="og:description" content="<?=$description;?>"><?}?>
+		<?if($image){?><meta property="og:image" content="<?=$GLOBALS['home'].$image;?>"><?}?>
+
+		<?if(isset($GLOBALS['facebook_api_id'])){?><meta property="fb:app_id" content="<?=$GLOBALS['facebook_api_id'];?>"><?}?>
+		
+		<?if(isset($GLOBALS['google_page'])){?><link href="<?=$GLOBALS['google_page'];?>" rel="publisher" /><?}?>
 
 
-<?
-include_once("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."header.php");
+		<?if(isset($GLOBALS['icons'])){?><link rel="stylesheet" href="<?=$GLOBALS['icons']?>"><?}?>
 
-echo"<div class='content".(isset($res['tpl']) ? " tpl-".encode($res['tpl']) : "")."'>";
+		<link rel="stylesheet" href="<?=$GLOBALS['path']?>api/global<?=$GLOBALS['min']?>.css?<?=$GLOBALS['cache']?>">	
 
-
-	if(isset($res['tpl'])) // On a une page
-	{
-		include("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."tpl/".$res['tpl'].".php");// On charge la template du thème pour afficher le contenu
-	}
-	else // Pas de contenu a chargé
-	{
-		echo"<div class='pal tc'>".$msg."</div>";
-	}
+		<link rel="stylesheet" href="<?=$GLOBALS['path']?>theme/<?=$GLOBALS['theme'].($GLOBALS['theme']?"/":"")?>style<?=$GLOBALS['min']?>.css?<?=$GLOBALS['cache']?>">	
 
 
-echo"</div>";
+		<?if(isset($GLOBALS['touch_icon'])){?><link rel="apple-touch-icon" href="<?=$GLOBALS['touch_icon'];?>"/><?}?>
+
+		<link rel="shortcut icon" type="image/x-icon" href="<?=$GLOBALS['path']?>media/favicon.ico?<?=$GLOBALS['cache']?>">
 
 
-include_once("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."/footer.php");
+		<script src="<?=$GLOBALS['jquery']?>"></script>
+
+		<script src="<?=$GLOBALS['path']?>api/lucide.init<?=$GLOBALS['min']?>.js?<?=$GLOBALS['cache']?>"></script>
+
+
+		<script>
+			<? if(isset($GLOBALS['google_analytics']) and $GLOBALS['google_analytics']) { ?>
+			// Google Analytics
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', '<?=$GLOBALS['google_analytics'];?>', 'auto');
+			ga('send', 'pageview');
+			<? }
+
+			if(isset($GLOBALS['facebook_sdk']) and $GLOBALS['facebook_api_id']) { ?>
+			// Facebook
+			(function(d, s, id){
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) {return;}
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.7&cookie=true&appId=<?=$GLOBALS['facebook_api_id'];?>";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+			<? } 
+
+			if(isset($_COOKIE['autoload_edit']) and $_SESSION['auth']['edit-page']){?>
+				// Si demande l'autoload du mode édition et si admin
+				$(function(){
+					edit_launcher();
+					$("a.bt.fixed.edit").fadeOut();				
+				});
+				<?
+				// Supprime le cookie qui demande de charger automatiquement l'admin
+				@setcookie("autoload_edit", "", time() - 3600, $GLOBALS['path'], $GLOBALS['domain']);
+			}?>			
+
+			// Variables
+			id = "<?=$id?>";
+			state = "<?=(isset($res['state'])?$res['state']:"")?>";
+			permalink = "<?=(isset($res['url'])?$res['url']:"")?>";
+			type = "<?=(isset($res['type'])?$res['type']:"")?>";
+			tpl = "<?=(isset($res['tpl'])?$res['tpl']:"")?>";
+			tag = "<?=(isset($tag)?encode($tag):"")?>";
+			path = "<?=$GLOBALS['path']?>";
+			theme = "<?=$GLOBALS['theme']?>";
+		</script>
+
+
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+	<body>
+	<?
+
+	include_once("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."header.php");
+
+	echo"<div class='content".(isset($res['tpl']) ? " tpl-".encode($res['tpl']) : "")."'>";
+}
+
+
+
+if(isset($res['tpl'])) // On a une page
+{
+	include("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."tpl/".$res['tpl'].".php");// On charge la template du thème pour afficher le contenu
+}
+else // Pas de contenu a chargé
+{
+	echo"<div class='pal tc'>".$msg."</div>";
+}
+
+
+
+// Si pas ajax on charge toute la page
+if(!$ajax)
+{
+	echo"</div>";
+
+
+	include_once("theme/".$GLOBALS['theme'].($GLOBALS['theme']?"/":"")."/footer.php");
+	?>
+
+	<script>console.log("<?=benchmark()?>")</script>
+	</body>
+	</html>
+	<? 
+}
+
+$connect->close();
 ?>
-
-<script>console.log("<?=benchmark()?>")</script>
-</body>
-</html>
-<? $connect->close(); ?>
