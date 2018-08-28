4938950dfbf5c219610f41777e428bc90bf5ef1b
simonvdfr <simonvd@hotmail.fr>
2018-08-28 12:12:23 +0200

    Ajout d'une condition pour désactiver à la demande les statistique analytics

--- index.php
+++ index.php
@@ -260,13 +260,17 @@ if(!$ajax)
 
 		<script>
 			<? if(@$GLOBALS['google_analytics']) { ?>
-			// Google Analytics
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', '<?=$GLOBALS['google_analytics'];?>', 'auto');
-			ga('send', 'pageview');
+			// Si Analytics pas desactivé
+			if(get_cookie('analytics') != "desactiver") 
+			{
+				// Google Analytics
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+				ga('create', '<?=$GLOBALS['google_analytics'];?>', 'auto');
+				ga('send', 'pageview');
+			}
 			<? }
 
 			if(@$GLOBALS['facebook_api_id']) { ?>
