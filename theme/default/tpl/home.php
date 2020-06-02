463e8fdfdfec433dc3c2d4134398d45adbe37f99
simonvdfr <simonvd@hotmail.fr>
2020-06-02 15:33:32 +0200

    centrage

--- theme/default/tpl/home.php
+++ theme/default/tpl/home.php
@@ -21,7 +21,7 @@
 		<article>
 			<div><?media('media-2', '130')?></div>
 			<h3 class="mbn tdn"><a <?href("lien-2")?>><?span('titre-2')?></a></h3>
-			<?txt('txt-2','w50 center')?>
+			<?txt('txt-2','w50 center block')?>
 			<div class="bt"><?_e("Lire plus")?></div>
 		</article>
 	</div>
@@ -30,7 +30,7 @@
 		<article>
 			<div><?media('media-3', '130')?></div>
 			<h3 class="mbn tdn"><a <?href("lien-3")?>><?span('titre-3')?></a></h3>
-			<?txt('txt-3','w50 center')?>
+			<?txt('txt-3','w50 center block')?>
 			<div class="bt"><?_e("Lire plus")?></div>
 		</article>
 	</div>
