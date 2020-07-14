824343e475d82581873f9ba12b1ae7581ef081ea
simonvdfr <simonvd@hotmail.fr>
2020-07-14 16:00:09 +0200

    suppression de l'image background editable

--- theme/default/tpl/page.php
+++ theme/default/tpl/page.php
@@ -1,11 +1,11 @@
 <?if(!$GLOBALS['domain']) exit;?>
 
-<section class="mw960p mod center" <?bg("fond")?>>
+<section class="mw960p mod center">
 
 	<?h1('title', 'mbn tc up color')?>
 	<?h2('sstitre', 'mbn tc big color-alt')?>
 
-	<article class="tj ptm pll prl">
+	<article class="tj pal ptm">
 
 		<?txt('texte')?>
 
