90a3c16a5c6aed68bd5c5757f79dd0ed0ebf0596
Simon Vandaele <simonvdfr@users.noreply.github.com>
2022-03-14 14:34:07 +0100

    clean les tags vide

--- api/ajax.admin.php
+++ api/ajax.admin.php
@@ -586,7 +586,7 @@ switch($_GET['mode'])
 
 				$i = 1;
 				foreach($tags as $cle => $val) {
-					if(isset($val) and $val != "") {		
+					if(isset($val) and trimer($val) != "") {		
 						$connect->query("INSERT INTO ".$table_tag." SET id='".(int)$_POST['id']."', zone='".$zone."', encode='".encode($val)."', name='".addslashes(trimer($val))."', ordre='".(isset($_POST['tag-ordre'])?(int)$_POST['tag-ordre']:$i)."'");
 						$i++;
 					}
