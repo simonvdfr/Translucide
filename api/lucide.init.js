84307b16d8418af93994c71a5ebc686b0796b4a0
Simon Vandaele <simonvdfr@users.noreply.github.com>
2022-04-19 17:44:36 +0200

    revu des boutons flottant pour plus d'accessibilité

--- api/lucide.init.js
+++ api/lucide.init.js
@@ -305,34 +305,35 @@ $(function()
 
 
 	// BOUTON EDITION | AJOUT
-	// Bouton ajout de page/article
-	$("body").append("<a href='javascript:void(0);' class='bt fixed add' title='"+ __("Add content") +"'><i class='fa fa-fw fa-plus bigger vam'></i></a>");
 
-	// Bind le bouton d'ajout
-	$("a.bt.add").click(function(){
-		add_content();
-	});	
-
-
-	// Bouton d'édition ou de connexion si la page existe dans la base
+	// BOUTON D'ÉDITION ou de connexion si la page existe dans la base
 	if(get_cookie("auth").indexOf("edit-page") > 0) var icon_edit = "pencil"; else var icon_edit = "key";// logé ou pas ?
-	if(typeof state !== 'undefined' && state) $("body").append("<a href='javascript:void(0);' class='bt fixed edit' title='"+ __("Edit the content of the page") +"'><i class='fa fa-fw fa-"+ icon_edit +" bigger vam'></i></a>");
+	if(typeof state !== 'undefined' && state) $("body").append("<button class='bt fixed edit' title='"+ __("Edit the content of the page") +"'><i class='fa fa-fw fa-"+ icon_edit +" bigger vam' aria-hidden='true'></i></button>");
 
 	// Bind le bouton d'édition
-	$("a.bt.edit").click(function() 
+	$(".bt.edit").click(function() 
 	{
 		// Si la page n'est pas activée et que l'on n'est pas admin on callback un reload
 		edit_launcher(((state != "active" && get_cookie("auth").indexOf("edit-page") < 0) ? "reload_edit":"edit_launcher"));
 
-		$("a.bt.fixed.edit").fadeOut();
+		$(".bt.fixed.edit").fadeOut();
 
 		// Force l'affichage du bouton  +
-		$("a.bt.fixed.add").show().css({"bottom":"10px", "opacity":".2"});
+		$(".bt.fixed.add").show().css({"bottom":"10px", "opacity":".2"});
 		edit_on = true;
 	});	
 
 
-	// Mode édition au ctrl+e
+	// BOUTON AJOUT de page/article
+	$("body").append("<button class='bt fixed add' title='"+ __("Add content") +"'><i class='fa fa-fw fa-plus bigger vam' aria-hidden='true'></i></button>");
+
+	// Bind le bouton d'ajout
+	$(".bt.add").click(function(){
+		add_content();
+	});	
+
+
+	// MODE ÉDITION AU CTRL+E
 	if(typeof shortcut !== 'undefined')
 	{
 		$(document).keydown(function(event) 
@@ -342,7 +343,7 @@ $(function()
 				if(event.ctrlKey || event.metaKey)
 				if(String.fromCharCode(event.which).toLowerCase() == 'e') {
 					event.preventDefault();
-					$("a.bt.edit").click();
+					$(".bt.edit").click();
 				}
 			}
 		});
@@ -350,23 +351,28 @@ $(function()
 
 
 	// Affichage du bouton add au survole du bt edition
-	$("a.bt.fixed.edit").hover(
-		function() {
-			$("a.bt.fixed.add").fadeIn();//fadeIn
-			$("a.bt.fixed.add").css("bottom", parseInt($("a.bt.fixed.edit").css("bottom")) + $("a.bt.fixed.edit").outerHeight() + "px");// au dessus bt edit
-			hover_add = true;
-		},
-		function() {
-			hover_add = false;
-			setTimeout(function() { if(!hover_add && !edit_on) $("a.bt.fixed.add").fadeOut("fast");	}, 1000);
-	});
+	$(".bt.fixed.edit").on("mouseenter mouseleave focusin",
+		function(event) {
+			if(event.type == "mouseenter" || event.type == "focusin")
+			{
+				$(".bt.fixed.add").fadeIn();//fadeIn
+				$(".bt.fixed.add").css("bottom", parseInt($(".bt.fixed.edit").css("bottom")) + $(".bt.fixed.edit").outerHeight() + "px");// au dessus bt edit
+				hover_add = true;
+			}
+			else if(event.type == "mouseleave")// || event.type == "focusout"
+			{
+				hover_add = false;
+				setTimeout(function() { if(!hover_add && !edit_on) $(".bt.fixed.add").fadeOut("fast");	}, 1000);
+			}
+		}
+	);
 	
 	// Onhover bouton add on le conserve visible
-	$("a.bt.fixed.add").hover(
+	$(".bt.fixed.add").hover(
 		function() { hover_add = true; },
 		function() {
 			hover_add = false;
-			setTimeout(function() { if(!hover_add && !edit_on && $("a.bt.fixed.edit").length) $("a.bt.fixed.add").fadeOut("fast");	}, 1000);
+			setTimeout(function() { if(!hover_add && !edit_on && $(".bt.fixed.edit").length) $(".bt.fixed.add").fadeOut("fast");	}, 1000);
 	});
 
 
@@ -377,29 +383,30 @@ $(function()
 		&& !$("#dialog-connect").length
 		&& typeof state !== 'undefined') 
 	{
-		if(state) $("a.bt.fixed.edit").delay("2000").fadeIn("slow");// get_cookie("auth").indexOf("edit-page")
-		else $("a.bt.fixed.add").delay("2000").fadeIn("slow");	
+		if(state) $(".bt.fixed.edit").delay("2000").fadeIn("slow");// get_cookie("auth").indexOf("edit-page")
+		else $(".bt.fixed.add").delay("2000").fadeIn("slow");	
 	}
 
 
 
 	// PAGE DÉSACTIVÉ => message admin
 	if(typeof state !== 'undefined' && state && state != "active" && get_cookie("auth").indexOf("edit-page") > 0) {
-		$("body").append("<a href='javascript:void(0);' class='bt fixed construction bold' title=\""+ __("Visitors do not see this content") +"\"><i class='fa fa-fw fa-attention vam no'></i> "+ __("Activation status") +" : "+ __(state) +"</a>");
+		$("body").append("<button class='bt fixed construction bold' title=\""+ __("Visitors do not see this content") +"\"><i class='fa fa-fw fa-attention vam no' aria-hidden='true'></i> "+ __("Activation status") +" : "+ __(state) +"</button>");
 		$(".bt.fixed.construction").click(function(){ $(this).slideUp(); });
 	}
 
 
 	
-	// BT TOP activé dans la config
+	// BT UP/TOP activé dans la config
 	if(typeof bt_top !== 'undefined')
 	{
 		// Bouton pour remonter en haut au scroll
-		$("body").append("<a href='javascript:void(0);' class='bt fixed top' title='"+ __("Back to Top") +"'><i class='fa fa-fw fa-up-open bigger'></i></a>");	
+		$("body").append("<button class='bt fixed top' title='"+ __("Back to Top") +"'><i class='fa fa-fw fa-up-open bigger' aria-hidden='true'></i></button>");	
 
 		// Smoothscroll to top
-		$("a.bt.fixed.top").click(function() {
+		$(".bt.fixed.top").click(function() {
 			$root.animate({scrollTop: 0}, 300);
+			$("a[href='#main']").focus();
 			return false;
 		});
 	}
@@ -444,8 +451,8 @@ $(function()
 		// AFFICHAGE DU BOUTON SCROLL TO TOP
 		if(typeof bt_top !== 'undefined')
 		{
-			if($window.scrollTop() > 50) $("a.bt.fixed.top").show();
-			else $("a.bt.fixed.top").fadeOut("fast");
+			if($window.scrollTop() > 50) $(".bt.fixed.top").show();
+			else $(".bt.fixed.top").fadeOut("fast");
 		}
 
 
