4f7681977a40dcfd86d19eb2656596cbf030624e
Thatoo <thatoo@users.noreply.github.com>
2022-05-27 13:51:59 +0200

    module recherche trouve l'url de la page recherche

--- theme/default/mdl/form-search.php
+++ theme/default/mdl/form-search.php
@@ -1,7 +1,15 @@
 <?php if(!$GLOBALS['domain']) exit;?>
 
 <!-- Formulaire de recherche -->
-	<form role="search" id="form-search" action="/search" method="post">
+<?php $sel_search = $connect->query("SELECT * FROM ".$table_content." WHERE tpl='search' LIMIT 1");
+if($connect->error) {
+	header($_SERVER['SERVER_PROTOCOL']." 503 Service Unavailable");
+	exit($connect->error);
+}
+else $res_search = $sel_search->fetch_assoc();
+ ?>
+
+	<form role="search" id="form-search" action="/<?php echo $res_search['url']; ?>" method="post">
 
 		<div id="input-search" class="inline-block">
 
