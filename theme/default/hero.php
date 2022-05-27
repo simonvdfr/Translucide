b699f85566cbc101810dc56637842672b7288d07
Thatoo <Thatoo@users.noreply.github.com>
2022-05-27 11:31:05 +0200

    ajout hero et breadcrump

---
+++ theme/default/hero.php
@@ -0,0 +1,14 @@
+<?php if(!$GLOBALS['domain']) exit;?>
+
+<section id="hero" class="layout-maxed bg-light">
+	<div class="layout-hero bg-section py-8" <?php bg('bg-hero')?>>
+		<div class="hero-content">
+			<div class="py-16">
+				<div class="hero-text text-center m-24 animation fade-in">
+					<?php h1('hero-title', 'hero-title color-blue')?>
+					<?php txt('hero-subtitle','hero-subtitle pt-20')?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
