74579f2b8c06e849793236c20f9401249a1ff8db
marionbouder <bouder.marion@pm.me>
2022-05-29 22:14:40 +0200

    maj hero files

--- theme/default/mdl/hero-article.php
+++ theme/default/mdl/hero-article.php
@@ -4,12 +4,15 @@
 	<div class="layout-hero bg-section bg-overlay py-8" <?php bg('bg-hero')?>>
 		<div class="hero-content">
 			<div class="py-16">
-				<?php if(isset($res['title'])){?>
 					<div class="hero-text text-center m-24 animation fade-in">
 						<h1 class="hero-title color-blue"><?=$res['title']?></h1>
 						<div class="hero-meta flex justify-center">
-							<?php if(isset($res['date_insert'])){?><div class="hero-meta-date-add px-8"><i class="icon moon-calendar"></i> <?php _e("Add the")?> <?=$res['date_insert']?></div><?php }?>
-							<?php if(isset($res['date_update'])){?><div class="hero-meta-date-update px-8"><i class="icon moon-calendar"></i> <?php _e("Updated the")?> <?=$res['date_update']?></div><?php }?>
+							<div class="hero-meta-date-add px-8">
+								<i class="icon moon-calendar"></i> <?php _e("Add the")?> <?=$res['date_insert']?>
+							</div>
+							<div class="hero-meta-date-update px-8">
+								<i class="icon moon-calendar"></i> <?php _e("Updated the")?> <?=$res['date_update']?>
+							</div>
 							<div class="hero-meta-author px-8">
 								<i class="icon moon-user"></i>
 								<?php
@@ -18,7 +21,7 @@
 							</div>
 						</div>
 					</div>
-				<?php }?>
+
 			</div>
 		</div>
 	</div>
--- theme/default/mdl/hero-page.php
+++ theme/default/mdl/hero-page.php
@@ -4,12 +4,10 @@
 	<div class="layout-hero bg-section py-8" <?php bg('bg-hero')?>>
 		<div class="hero-content">
 			<div class="py-16">
-				<?php if(isset($res['title'])){?>
 					<div class="hero-text text-center m-24 animation fade-in">
 						<h1 class="hero-title color-blue"><?=$res['title']?></h1>
 						<?php txt('hero-subtitle','hero-subtitle pt-20')?>
 					</div>
-				<?php }?>
 			</div>
 		</div>
 	</div>
