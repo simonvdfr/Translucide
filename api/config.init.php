41cc676f90919840d1a73a26d83f9e1168fb5a17
Simon Vandaele <simonvdfr@users.noreply.github.com>
2023-02-07 18:21:52 +0100

    correction pour l'installation du CMS dans un sous-dossier d'un domaine

--- api/config.init.php
+++ api/config.init.php
@@ -343,7 +343,7 @@ $GLOBALS['style.css'] = null;
 
 
 // Librairie externe
-$GLOBALS['jquery'] = '/api/jquery.min.js';// //ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+$GLOBALS['jquery'] = $GLOBALS['path'].'api/jquery.min.js';// //ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 
 $GLOBALS['jquery_ui'] = '//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js';
 
